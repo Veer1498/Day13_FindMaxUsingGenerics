c81586ba4f98d3463102b5361146cabc0936c26a
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-11 16:53:40 +0530

    Added: Ability to find Max of 3 Integers

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Find Maxmum Program");
-	//	FindMaxUsingGenerics findMax = new FindMaxUsingGenerics();
+		FindMaxUsingGenerics findMax = new FindMaxUsingGenerics();
 		
-	//	findMax.getMax(10, 20, 30);
+		findMax.getMax(10, 20, 30);
 
 	}
 
