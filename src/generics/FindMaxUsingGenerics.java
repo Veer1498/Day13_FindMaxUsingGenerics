3352381d4973282d75aae3da84c20a21a08b3a29
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-11 18:10:59 +0530

    Added : Ability to find Max Value Whatever the Input DataType Maybe

--- src/generics/FindMaxUsingGenerics.java
+++ src/generics/FindMaxUsingGenerics.java
@@ -2,10 +2,10 @@ package generics;
 
 public class FindMaxUsingGenerics {
 
-	public static String getMax(String x, String y, String z) {
+	public static <S extends Comparable> S getMax(S x, S y, S z) {
 		// TODO Auto-generated method stub
 		
-		String max = x;
+		S max = x;
 		if(y.compareTo(x)>0 && y.compareTo(z)>0)
 			max = y;
 		else if(z.compareTo(y)>0 && z.compareTo(x)>0)
