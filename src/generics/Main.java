664c9dee326d4e54b82fe1a57a72eb6b51c7556f
Veer1498 <singavarapuveerendra@gmail.com>
2022-07-11 17:46:38 +0530

    Added: Ability to find max value at 3 Positions

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		System.out.println("Welcome to Find Maxmum Program");
 		FindMaxUsingGenerics findMax = new FindMaxUsingGenerics();
 		
+		findMax.getMax(30, 20, 10);
+		findMax.getMax(10, 30, 20);
 		findMax.getMax(10, 20, 30);
 
 	}
